bd0a0a7adb8752f69c6068be47aac104ffc82f29
Andrew Wayne Valentine <andrew.w.valentine7.mil@swf.army.mil>
2022-08-10 14:54:18 -0500

    initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+    public static void main(String[] args) {
+
+        double decimalNum = 88.5;
+        int intNum = 6;
+        double sum = decimalNum*intNum;
+        System.out.println("Hello to someone professional");
+        System.out.println(decimalNum);
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/com/galvanize/PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize;
+
+public class PracticeApplication {
+    public static void main(String[] args) {
+
+        for (int age = 21; age < 30; age++) {
+            if (age > 27) {
+                System.out.println(age);
+            }
+
+        }
+    }
+}
